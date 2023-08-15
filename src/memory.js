ec20108a6904cad0d461a73d338a41a74d9e039c
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-08-15 10:36:57 +0200

    iteration2

--- src/memory.js
+++ src/memory.js
@@ -4,19 +4,39 @@ class MemoryGame {
     // add the rest of the class properties here
     this.pickedCards = [];
     this.pairsClicked = 0;
-    this.pairsGueseed = 0;
+    this.pairsGuessed = 0;
     
   }
 
   shuffleCards() {
     // ... write your code here
+    let copyCards = [...this.cards]
+    let shuffledCardsArr = []
+    for (let i = 0; this.cards.length; i+=1) {
+      let randomIndex = Math.floor(Math.random() * copyCards.length)
+      shuffledCardsArr.push(copyCards[randomIndex])
+      copyCards.splice(randomIndex, 1)
+    }
+    this.cards = shuffledCardsArr 
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+   this.pairsClicked++
+   if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+   } else {
+      return false
+   }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed >= 12) {
+      return true
+    } else {
+      return false
+    }
   }
 }
